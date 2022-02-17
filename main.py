b8de6520c7f2a92c6d2f4a14914a43e841c68588
Ronaldo Rueda <rueda.ronaldo@hotmail.com>
2022-02-16 23:39:08 -0600

    Found article that makes it easier to get csv into a data objects.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import csv
+import pandas as pd
 
-file = open('test2.CSV')
-type(file)
+data = pd.read_csv('test2.csv')
+print(data)
+print(data.Subject)             #can use data.[column name] to print that whole column. very helpful...
 
+#using article https://www.analyticsvidhya.com/blog/2021/08/python-tutorial-working-with-csv-file-for-data-science/
