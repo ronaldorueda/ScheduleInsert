5c93558f5c4b9575f58768fc58332c811eeea9a1
Ronaldo Rueda <rueda.ronaldo@hotmail.com>
2022-02-18 16:43:19 -0600

    Created variables needed to set into to later loop through them. might not be needed but just done for now.

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ data = pd.read_csv('test2.csv')
 print(data)
 print(data.Subject)             #can use data.[column name] to print that whole column. very helpful...
 
+clientList = list() #will gran subject to let us know who the client is
+dateList = list()
+beginningTimeList = list()
+endTimeList = list()
+descriptionList = list() #will grab description to grab link to enter time entries / should also have small description to write to time entries
+
+
+
 #using article https://www.analyticsvidhya.com/blog/2021/08/python-tutorial-working-with-csv-file-for-data-science/
