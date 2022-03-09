7d9e5e37906644ad35c205028e84afbf252e0e87
Ronaldo Rueda <rueda.ronaldo@hotmail.com>
2022-03-08 20:40:05 -0600

    loop methods to get data and add to list. documentation has been updated.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import pandas as pd
+import pandas as pd                 #pandas is a tool to go through csv files
+from datetime import date           #needed to get today's date since it will always be ran at the end of day
 
 data = pd.read_csv('test2.csv')
-print(data)
-print(data.Subject)             #can use data.[column name] to print that whole column. very helpful...
+#print(data)
+#print(data.Subject)                #can use data.[column name] to print that whole column. very helpful...
 
-clientList = list() #will gran subject to let us know who the client is
-dateList = list()
-beginningTimeList = list()
-endTimeList = list()
-descriptionList = list() #will grab description to grab link to enter time entries / should also have small description to write to time entries
+clientList = list()                 #will grab subject to let us know who the client is
+todaysDate = date.today()           #today's date
+beginningTimeList = list()          #beginning time of each item in calendar
+endTimeList = list()                #end time of each item in calendar
+descriptionList = list()            #will grab description to grab link to enter time entries / should also have small description to write to time entries
+siteList = list()                   #grab the site these entries need to be made to
 
+#loop through Subject from csv and append to clientList
+for item in data.Subject:
+    clientList.append(item)
 
+#loop to get start times and add to beginList
+for item in data.StartTime:
+    beginningTimeList.append(item)
 
-#using article https://www.analyticsvidhya.com/blog/2021/08/python-tutorial-working-with-csv-file-for-data-science/
+#loop to get end times and add to endList
+for item in data.EndTime:
+    endTimeList.append(item)
+
+#loop to get descriptions and sites to their lists
+for item in data.Description:
+    descriptionList.append(item[0:item.index(';')])
+    siteList.append(item[(item.index(';') + 1) : (len(item) - 1)])
+
--- readme.txt
+++ readme.txt
@@ -1,5 +1,20 @@
-This repository is for automate time entries based off of Calendar exports.
+This repository is to automate time entries based off of Outlook calendar csv exports. The problem that had led me to begin this project is the excessive amount of time it takes to perform time entries. 
+Management failed to listen to our cries and criticism with the current system, so I am taking actions on my own hands.
+In the dev team, we do a good job of planning our day in our Outlook calendars to let team members and management know what we are working on the hour, and I want to take that export and automate time entries.
 
-This script will go through the csv and grab time and dates off and enter them into specified Smartsheets.
+This script will go through the csv export and grab time and dates off and enter them into specified Smartsheets or Base99 tickets.
+Currently the script is able to loop through the csv file based off the headers and assigning them to respective lists. After doing minimal research, I was not able to find a better solution to loop through rows via Pandas.
+For now, as mentioned priorly, I use multiple for loops to go through each header that I need to get data from.
+For the time being, I believe it is not ideal to create a class with the respected fields, but subject to change as I continue to chug along as I do not like this current approach.
 
 More exact documentation as this grows will be added.
+
+The documentation I have used to get started was based off the following article:
+https://www.analyticsvidhya.com/blog/2021/08/python-tutorial-working-with-csv-file-for-data-science/
+
+URL for Pandas:
+https://pandas.pydata.org/docs/index.html
+
+I am always open to feedback and criticism (seriously please criticize me and make me do better as I want to improve)
+
+Thanks!
